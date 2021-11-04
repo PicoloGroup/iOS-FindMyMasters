04606ea129e2d701d5e68da4e866c77fca3ad6de
Hasan Can Aslan <aslan4044@gmail.com>
2021-11-04 23:08:26 +0300

    feat: implement login view controller

---
+++ FindMyMasters/Models/LoginDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  LoginDelegate.swift
+//  FindMyMasters
+//
+//  Created by Hasan Can Aslan on 28.10.2021.
+//
+
+import Foundation
+
+protocol LoginDelegate: NSObject {
+    func loginDidOccur()
+}

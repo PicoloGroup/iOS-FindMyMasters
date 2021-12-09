d0b21f10e125f431cc2470fba89be9d018982bcb
Hasan Can Aslan <aslan4044@gmail.com>
2021-12-09 17:50:46 +0300

    feat: add user defaults backed data typle

---
+++ FindMyMasters/Core/Models/UserDefaultsBacked.swift
@@ -0,0 +1,46 @@
+//
+//  UserDefaultsBacked.swift
+//  FindMyMasters
+//
+//  Created by Hasan Can Aslan on 9.12.2021.
+//
+
+import Foundation
+
+// property wrapper's Value type isn't optional, but
+// can still contain nil values, we'll have to introduce this
+// protocol to enable us to cast any assigned value into a type
+private protocol AnyOptional {
+    var isNil: Bool { get }
+}
+
+extension Optional: AnyOptional {
+    var isNil: Bool { self == nil }
+}
+
+@propertyWrapper
+struct UserDefaultsBacked<Value> {
+    let key: String
+    let defaultValue: Value
+    var storage: UserDefaults = .standard
+
+    var wrappedValue: Value {
+        get {
+            let value = storage.value(forKey: key) as? Value
+            return value ?? defaultValue
+        }
+        set {
+            if let optional = newValue as? AnyOptional, optional.isNil {
+                storage.removeObject(forKey: key)
+            } else {
+                storage.setValue(newValue, forKey: key)
+            }
+        }
+    }
+}
+
+extension UserDefaultsBacked where Value: ExpressibleByNilLiteral {
+    init(key: String, storage: UserDefaults = .standard) {
+        self.init(key: key, defaultValue: nil, storage: storage)
+    }
+}
